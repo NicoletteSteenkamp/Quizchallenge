892688679beddf91cf8cdc4f252e93dda6b4fffb
Aororiswep <136186748+Aororiswep@users.noreply.github.com>
2023-12-04 18:55:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,161 +1,104 @@
-const questionContainer = document.getElementById("question-container");
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
-const restartButton = document.getElementById("restart-btn");
-const resultDiv = document.getElementById("result");
 
-let shuffledQuestions, currentQuestionIndex, score;
 
-const questions = [
-  {
-    question: "What is 2 + 2?",
-    answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
-      { text: "222", correct: false },
-      { text: "2222", correct: false },
-    ],
-  },
-  {
-    question: "What does HTML stand for?",
-    answers: [
-      { text: "HyperText Markup Language", correct: true },
-      { text: "HighText Machine Language", correct: false },
-      { text: "HyperText Machine Language", correct: false },
-      { text: "HighText Markup Language", correct: false },
-    ],
-  },
-  {
-    question: "Which property is used to change the background color?",
-    answers: [
-      { text: "color", correct: false },
-      { text: "bgColor", correct: false },
-      { text: "background-color", correct: true },
-      { text: "background", correct: false },
-    ],
-  },
-  {
-    question: "Inside which HTML element do we put the JavaScript?",
-    answers: [
-      { text: "<js>", correct: false },
-      { text: "<javascript>", correct: false },
-      { text: "<script>", correct: true },
-      { text: "<scripting>", correct: false },
-    ],
-  },
-      {
-    question: "Which tag defines the head of a document?",
-    answers: [
-      { text: "<title>", correct: false },
-      { text: "<head>", correct: true },
-      { text: "<body>", correct: false },
-      { text: "<header>", correct: false },
-    ],
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  const questionContainer = document.getElementById("question-container");
+  const questionElement = document.getElementById("question");
+  const answerButtons = document.getElementById("answer-buttons");
+  const nextButton = document.getElementById("next-btn");
+  const restartButton = document.getElementById("restart-btn");
+  const resultDiv = document.getElementById("result");
 
-  {
-    question: "What is the correct way to link an external stylesheet?",
-    answers: [
-      { text: `<link href="style.css" rel="stylesheet">`, correct: true },
-      { text: `<style src="style.css"></style>`, correct: false },
-      { text: `<import href="style.css">`, correct: false },
-      { text: `<style link ="style.css" rel="stylesheet">`, correct: false },
-    ],
-  },
+  let shuffledQuestions, currentQuestionIndex, score;
 
-  {
-    question: "What property sets the spacing between elements?",
-    answers: [
-      { text: "margin", correct: true },
-      { text: "padding", correct: false },
-      { text: "space", correct: false },
-      { text: "border", correct: false },
-    ],
-  },
+  startQuiz();
 
-  {
-    question: "How do you center an element horizontally?",
-    answers: [
-      { text: "text-align: center;", correct: true },
-      { text: "align: center;", correct: false },
-      { text: "margin: auto;", correct: false },
-      { text: "center: true", correct: false },
-    ],
+  function startQuiz() {
+    score = 0;
+    questionContainer.style.display = "block";
+    shuffledQuestions = shuffle(quizQuestions);
+    currentQuestionIndex = 0;
+    nextButton.classList.remove("hide");
+    restartButton.classList.add("hide");
+    resultDiv.classList.add("hide");
+    setNextQuestion();
   }
-];
 
-startQuiz();
-
-function startQuiz() {
-  score = 0;
-  questionContainer.style.display = "flex";
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0;
-  nextButton.classList.remove("hide");
-  restartButton.classList.add("hide");
-  resultDiv.classList.add("hide");
-  setNextQuestion();
-}
-
-function setNextQuestion() {
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question;
-  question.answers.forEach((answer, index) => {
-    const inputGroup = document.createElement("div");
-    inputGroup.classList.add("input-group");
-
-    const radio = document.createElement("input");
-    radio.type = "radio";
-    radio.id = "answer" + index;
-    radio.name = "answer";
-    radio.value = index;
-
-    const label = document.createElement("label");
-    label.htmlFor = "answer" + index;
-    label.innerText = answer.text;
-
-    inputGroup.appendChild(radio);
-    inputGroup.appendChild(label);
-    answerButtons.appendChild(inputGroup);
-  });
-}
-
-function resetState() {
-  while (answerButtons.firstChild) {
-    answerButtons.removeChild(answerButtons.firstChild);
+  function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
   }
-}
 
-nextButton.addEventListener("click", () => {
-  const answerIndex = Array.from(
-    answerButtons.querySelectorAll("input")
-  ).findIndex((radio) => radio.checked);
-  if (answerIndex !== -1) {
-    if (shuffledQuestions[currentQuestionIndex].answers[answerIndex].correct) {
+  function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.options.forEach((option) => {
+      const button = document.createElement("button");
+      button.classList.add("btn");
+      button.textContent = option.text;
+      button.addEventListener("click", () => selectAnswer(option));
+      answerButtons.appendChild(button);
+    });
+  }
+
+  function resetState() {
+    while (answerButtons.firstChild) {
+      answerButtons.removeChild(answerButtons.firstChild);
+    }
+    nextButton.classList.add("hide");
+    resultDiv.classList.add("hide");
+    answerButtons.classList.remove("disable-click");
+  }
+
+  function selectAnswer(selectedOption) {
+    const question = shuffledQuestions[currentQuestionIndex];
+    const correct = selectedOption.correct;
+
+    if (correct) {
       score++;
     }
+
+    answerButtons.classList.add("disable-click");
+    showCorrectAnswer(question, selectedOption);
+
     currentQuestionIndex++;
-    if (shuffledQuestions.length > currentQuestionIndex) {
-      setNextQuestion();
+    if (currentQuestionIndex < shuffledQuestions.length) {
+      setTimeout(setNextQuestion, 2000);
     } else {
-      endQuiz();
+      setTimeout(endQuiz, 2000);
     }
-  } else {
-    alert("Please select an answer.");
   }
+
+  function showCorrectAnswer(question, selectedOption) {
+    const correctIndex = question.options.findIndex((option) => option.correct);
+    answerButtons.children[correctIndex].classList.add("correct");
+
+    if (selectedOption.correct) {
+      resultDiv.innerText = "Correct!";
+      resultDiv.classList.add("correct");
+    } else {
+      resultDiv.innerText = "Incorrect!";
+      resultDiv.classList.add("incorrect");
+    }
+
+    resultDiv.classList.remove("hide");
+    restartButton.classList.add("hide");
+    nextButton.classList.add("hide");
+  }
+
+  function endQuiz() {
+    questionContainer.style.display = "none";
+    resultDiv.classList.remove("hide");
+    resultDiv.innerText = `Your final score: ${score} / ${shuffledQuestions.length}`;
+    restartButton.classList.remove("hide");
+  }
+
+  function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  nextButton.addEventListener("click", () => {
+    answerButtons.classList.remove("disable-click");
+    resultDiv.classList.add("hide");
+  });
+
+  restartButton.addEventListener("click", startQuiz);
 });
-
-restartButton.addEventListener("click", startQuiz);
-
-function endQuiz() {
-  questionContainer.style.display = "none";
-  nextButton.classList.add("hide");
-  restartButton.classList.remove("hide");
-  resultDiv.classList.remove("hide");
-  resultDiv.innerText = `Your final score: ${score} / ${shuffledQuestions.length}`;
-}
